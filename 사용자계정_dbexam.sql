e560cf38695d7193a09b5d5d786fb58c200ef4fa
철팽 <124338748+CheolPang@users.noreply.github.com>
2026-06-04 17:50:31 +0900

    Add files via upload

--- 사용자계정_dbexam.sql
+++ 사용자계정_dbexam.sql
@@ -1,135 +1,117 @@
------[1. dbtable 생성]-----
-select * from tab;
-create table dbtest(
-    name varchar2(15), 
-    age number,
-    height number(5,2),
-    logtime date
-);
-desc dbtest;
-drop table dbtest;
-flashback table dbtest to before drop;
-
-insert into dbtest (name, age, height, logtime) values ('Park-JinYeong',25, 185.445, sysdate);
-insert into dbtest (name, age, logtime) values ('Park-TwoYeong',25, sysdate);
-select * from dbtest;
-select name, logtime from dbtest;
-insert into dbtest values ('Kim-DongRul',27, 176.46, sysdate);
-insert into dbtest values ('IU',30, 166, sysdate);
-insert into dbtest values ('HongDang-Moo',45, 176.5, sysdate);
-insert into dbtest values ('Hong-GilDong',50, 156.5, sysdate);
-insert into dbtest values ('Kim-HongSuk',40, 156.5, sysdate);
-insert into dbtest values ('Na-Hong',50, 166.5, sysdate);
-insert into dbtest values ('NaNa-Hong',50, 166.5, sysdate);
-insert into dbtest (name, age, height) values ('hong si',45, 176.5);
-insert into dbtest (age, height) values (45, 176.5);
-insert into dbtest (name, height) values ('Kim-NaNa', 176.5);
-select count(*) from dbtest;
-select count(height) from dbtest;
-select * from dbtest;
-commit;
-
---정렬
-select * from dbtest order by name asc; --기본이 오름차순이라 asc를 생략해도 됨. 다만 내림차순을 할 때에는 desc를 쓰면 됨.
-select * from dbtest order by name desc;
-select * from dbtest order by age, height desc;
-
---조건 검색
-select * from dbtest where name='HongDang-Moo';
-select * from dbtest where name='HONG SI'; --문자열은 대소문자 구분을 하기 때문에 문자열을 검색할 때에는 대소문자 구분을 잘 해서 넣어야 한다.
-select * from dbtest where name='hong si';
-select * from dbtest where name='Hong'; -- 홍이 포함된 사람을 찾는 것이 아니기 때문에 검색하려는 대상을 정확하게 입력하여야 한다.
-select * from dbtest where name like '%Hong%'; --홍이 포함된 사람을 찾을 때는 = 대신 like를 쓰고, 모든 조건에서 다 찾으려면 문자열 양 옆에 %를 붙여야 한다.
-select * from dbtest where name like 'Hong%'; --홍으로 시작하는 사람만 찾는다.
-select * from dbtest where name like '%Hong'; --홍으로 끝나는 사람만 찾는다.
-select * from dbtest where name like '___Hong'; --언더바는 글자수를 뜻한다.
-select * from dbtest where name like '_____Hong';
-select * from dbtest where name like '%Hong%' and age>=45; --조건이 두가지다. Hong이 들어가고, 동시에 나이가 45 이상인 것을 찾는다.
-select * from dbtest where height is null; --height 데이터가 null인것만 찾는다.
-select * from dbtest where height is not null; --height 데이터가 null이지 않은 것만 찾는다.
-
---(문제) 한 사람의 데이터 중 어느 하나에 컬럼에 null이 들어가 있으면 모두 출력하시오.
-select * from dbtest where name is null or age is null or height is null or sysdate is null; --or 조건 주기
-commit;
-select * from dbtest;
-
---삭제
-delete dbtest where name='HongDang-Moo'; --delete from 에서 from은 생략 가능.
-delete dbtest; --모든 데이터를 삭제할 수 있음 (테이블은 삭제하지 않음. 테이블 삭제 시 drop table; 사용) / 마지막에 * 생략 가능.
---이름이 김으로 시작하는 사람들을 모두 삭제
-delete dbtest where name like 'Kim%';
-rollback;
-
---수정
-update dbtest set age=age+1 where name like 'Hong%';
-update dbtest set age=age+1 where name like '%Hong%';
-select * from dbtest;
-update dbtest set age=30 where name='Hong-GilDong';
---나이가 null인 사람의 데이터는 모두 30살로 수정
-update dbtest set age=30 where age is null;
-
---depart 부서 테이블 생성
-create table depart(
-    deptno number not null, --부서번호(학과번호)
-    dname varchar2(25) not null, --부서명(학과명)
-    loc varchar2(10) default null --위치(건물명)  
+----------------[3.p73_selfEX Quiz]---------------
+create table department(
+deptno number not null, --부서번호(학과번호)
+dname varchar2(25) not null, --부서명(학과명)
+loc varchar2(10) default null --위치(건물명)
 );
 
 
-select * from depart;
+select * from department;
 ----drop table depart purge;
 
 
-INSERT INTO depart (deptno, dname) VALUES (302, 'Electrical_Engineering');
-INSERT INTO depart VALUES (101, 'Computer_Science', 'Building1');
-INSERT INTO depart VALUES (102, 'Multimedia', 'Building2');
-INSERT INTO depart VALUES (201, 'Electronic_Engineering', 'Building3');
-INSERT INTO depart VALUES (202, 'Mechanical_Engineering', 'Building4');
+INSERT INTO department(deptno, dname) VALUES (302, '전기공학과');
+INSERT INTO departmentVALUES (101, '컴퓨터공학과', '1호관');
+INSERT INTO departmentVALUES (102, '멀티미디어학과', '2호관');
+INSERT INTO departmentVALUES (201, '전자공학과', '3호관');
+INSERT INTO departmentVALUES (202, '기계공학과', '4호관');
+
+-- professor 테이블
+CREATE TABLE professor (
+profno number NOT NULL,       -- 교수번호
+name varchar2(20) NOT NULL,   -- 이름
+userid varchar2(10) NOT NULL, -- 아이디
+position varchar2(20) NOT NULL,   -- 직급
+sal number NOT NULL,          -- 급여
+hiredate date NOT NULL,           -- 입사일
+comm number DEFAULT NULL, -- 보직수당
+deptno number NOT NULL,       -- 부서번호(학과번호)
+PRIMARY KEY (profno));    
 
 
---emp 테이블
-create table emp(
-    empno number primary key, --직원번호 pk:unique(데이터 중복 금지), not null(반드시 데이터가 있어야 한다.)
-    name varchar2(20) not null, --이름
-    position varchar2(10) not null, --직급
-    tel varchar2(15) not null, --연락처
-    deptno number not null); --학과번호
-    --empYear number, --연차
-    --regTime date --입사 날짜
-    --primary key(empno)
-
-
-drop table emp purge;
-
-
---INSERT INTO emp (empno,name,position,tel,deptno,empYear) VALUES (20101, '홍길동', '사원', '031)781-2158', 101, null);
-INSERT INTO emp VALUES (20101, 'Hong-GilDong', 'Sawon', '031)781-2158', 101);
-INSERT INTO emp VALUES (10102, 'Kim-CheolSu', 'GwaZang', '032)261-8947', 101);
-INSERT INTO emp VALUES (10103, 'Lee-YeongHee', 'Daeri', '02)824-9637', 102);
-INSERT INTO emp VALUES (10104, 'Go-GilDong', 'Sawon', '02)824-9637', 102);
-INSERT INTO emp VALUES (10105, 'Kang-HoDong', 'Sawon', '02)824-9637', 102);
-INSERT INTO emp VALUES (10106, 'IU', 'Sawon', '02)881-2158', 105);
-
---중복값을 방지하기 위해 자동으로 순차적으로 증강하는 순변을 생성하는 데이터베이스 객체 => 시퀀스
-create sequence seq_board nocycle nocache;
-drop sequence seq_board;
-
-create sequence empno nocycle nocache;
-INSERT INTO emp VALUES (empno.nextval, '김사원', '대리', '02)881-2558', 105, sysdate);select * from seq;
-INSERT INTO emp VALUES (empno.nextval, '김대리', '사원', '02)881-2188', 105, sysdate);select * from seq;
+INSERT INTO professor VALUES
+(9901, '김도훈', 'capool', '교수', 500,
+To_Date( '2010.12.08 16:33', 'yyyy.mm.dd hh24:mi'), 20, 101);
+INSERT INTO professor VALUES
+(9902, '이재우', 'sweat413', '조교수', 320,
+To_Date( '1995.04.12 00:00:00', 'yyyy.mm.dd hh24:mi:ss'), null, 201);
+INSERT INTO professor VALUES
+(9903, '성연희', 'pascal', '조교수', 360,
+To_Date('1993.03.17', 'YYYY.MM.DD'), 15, 101);
+INSERT INTO professor VALUES
+(9904, '염일웅', 'blue77', '전임강사', 240,
+To_Date('1998-10-11 00:00:00', 'yyyy-mm-dd hh24:mi:ss'), null, 102);
+INSERT INTO professor VALUES
+(9905, '권혁일', 'refresh', '교수', 450,  
+To_Date('1986-02-11 00:00:00', 'yyyy-mm-dd hh24:mi:ss'), 25, 102);
+INSERT INTO professor VALUES
+(9906, '이만식', 'pocari', '부교수', 420,  
+To_Date('1988-07-11 00:00:00', 'yyyy-mm-dd hh24:mi:ss'), null, 101);
+INSERT INTO professor VALUES
+(9907, '전은지', 'totoro', '전임강사', 210,  
+To_Date('2001-05-11 00:00:00', 'yyyy-mm-dd hh24:mi:ss'), null, 101);
+INSERT INTO professor VALUES
+(9908, '남은혁', 'bird13', '부교수', 400,  
+To_Date('1990-10-18 00:00:00', 'yyyy-mm-dd hh24:mi:ss'), 17, 202);
 
 
 
-select * from emp;
-select empno, sysdate from emp;
-select empno, to_char(sysdate, 'YY-mm-dd hh:mi:ss') from emp;
-select empno, to_char(sysdate, 'YY-mm-dd hh24:mi:ss') from emp;
-select empno, to_char(sysdate, '""YYYY"년 "mm"월 "dd"일 "hh"시 "mi"분 "ss"초') from emp;
-select * from depart;
+--student 테이블
+CREATE TABLE student (
+studno number NOT NULL,       -- 학생번호
+name varchar2(20) NOT NULL,   -- 이름
+userid varchar2(10) NOT NULL,     -- 아이디
+grade number NOT NULL,        -- 학년
+idnum varchar2(13) NOT NULL,  -- 주민번호
+birthdate date NOT NULL,      -- 생년월일
+tel varchar2(15) NOT NULL,    -- 전화번호
+height number NOT NULL,       -- 키
+weight number NOT NULL,       -- 몸무게
+deptno number NOT NULL,       -- 학과번호
+profno number DEFAULT NULL,   -- 담당교수의 일련번호
+PRIMARY KEY (studno)) ;
+
+
+INSERT INTO student VALUES
+(10101, '전인하', 'jun123', 4, '7907021369824', To_Date('1979-07-02', 'yyyy-mm-dd'), '051)781-2158', 176, 72, 101, 9903);
+INSERT INTO student VALUES
+(10102, '박미경', 'ansel414', 1, '8405162123648', To_Date('1984-05-16', 'yyyy-mm-dd'), '055)261-8947', 168, 52, 101, null);
+INSERT INTO student VALUES
+(10103, '김영균', 'mandu', 3, '8103211063421', To_Date('1981-03-21', 'yyyy-mm-dd'), '051)824-9637', 170, 88, 101, 9906);
+INSERT INTO student VALUES
+(10104, '지은경', 'gomo00', 2, '8004122298371', To_Date('1980-04-12', 'yyyy-mm-dd'), '055)418-9627', 161, 42, 101, 9907);
+INSERT INTO student VALUES
+(10105, '임유진', 'youjin12', 2, '8301212196482', To_Date('1983-01-21', 'yyyy-mm-dd'), '02)312-9838', 171, 54, 101, 9907);
+INSERT INTO student VALUES
+(10106, '서재진', 'seolly', 1, '8511291186273', To_Date('1985-11-29', 'yyyy-mm-dd'), '051)239-4861', 186, 72, 101, null);
+INSERT INTO student VALUES
+(10107, '이광훈', 'huriky', 4, '8109131276431', To_Date('1981-09-13', 'yyyy-mm-dd'), '055)736-4981', 175, 92, 101, 9903);
+INSERT INTO student VALUES
+(10108, '류민정', 'cleansky', 2, '8108192157498', To_Date('1981-08-19', 'yyyy-mm-dd'), '055)248-3679', 162, 72, 101, 9907);
+INSERT INTO student VALUES
+(10201, '김진영', 'simply', 2, '8206062186327', To_Date('1982-06-06', 'yyyy-mm-dd'), '055)419-6328', 164, 48, 102, 9905);
+INSERT INTO student VALUES
+(10202, '오유석', 'yousuk', 4, '7709121128379', To_Date('1977-09-12', 'yyyy-mm-dd'), '051)724-9618', 177, 92, 102, 9905);
+INSERT INTO student VALUES
+(10203, '하나리', 'hanal', 1, '8501092378641', To_Date('1985-01-09', 'yyyy-mm-dd'), '055)296-3784', 160, 68, 102, null);
+INSERT INTO student VALUES
+(10204, '윤진욱', 'samba7', 3, '7904021358671', To_Date('1979-04-02', 'yyyy-mm-dd'), '053)487-2698', 171, 70, 102, 9905);
+INSERT INTO student VALUES
+(20101, '이동훈', 'dals', 1, '8312101128467', To_Date('1983-12-10', 'yyyy-mm-dd'), '055)426-1752', 172, 64, 201, null);
+INSERT INTO student VALUES
+(20102, '박동진', 'ping2', 1, '8511241639826', To_Date('1985-11-24', 'yyyy-mm-dd'), '051)742-6384', 182, 70, 201, null);
+INSERT INTO student VALUES
+(20103, '김진경', 'lovely', 2, '8302282169387', To_Date('1983-02-28', 'yyyy-mm-dd'), '052)175-3941', 166, 51, 201, 9902);
+INSERT INTO student VALUES
+(20104, '조명훈', 'rader214', 1, '8412141254963', To_Date('1984-12-14', 'yyyy-mm-dd'), '02)785-6984', 184, 62, 201, null);
+
+
+select * from student;
+select * from professor;
 commit;
 
+select name, sal ,plus from professor;
 
---self Util
 select * from users;
 create table users(
     ID varchar2(4000), 
@@ -139,10 +121,26 @@ create table users(
     BAN number(1),
     logtime date
 );
-drop table users;
 
-insert into users values ('TestAccount','1234', '테스트계정', 0, 0, sysdate);
-delete users where ID='TestAccount2';
-update users set PW='Ne12312t' where ID='TestAccount' AND PW='1234';
-update users set WARN=WARN+1 where ID='TestAccount';
+
+
+---------------[school data]-----------------
+--table 작성
+create table school(
+    name VARCHAR2(15) not null,
+    value VARCHAR2(30),
+    code number,--1이면 학생, 2이면 교수, 3이면 관리자
+    logtime date
+);
+---drop table school purge;
+desc school;
+select * from tab;
+
+
+insert into school values('홍길동', '2018001', 1,sysdate);
+insert into school values('홍길선', '컴퓨터공학부', 2,sysdate);
+insert into school values('김길관', '관리부', 3,sysdate);
+select * from school;
+select * from school where name like '%홍%';
+delete school where name='홍길동';
 commit;
